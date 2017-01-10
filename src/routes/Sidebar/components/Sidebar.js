932e11d5dcaabf6b2762e9c7453565b8735b716f
Steven Bone <steven@stevenbone.com>
2017-01-09 22:37:45 -0500

    Sidebar styles

--- src/routes/Sidebar/components/Sidebar.js
+++ src/routes/Sidebar/components/Sidebar.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import './../Sidebar.scss'
 
 export const Sidebar = (props) => {
   console.log(props.isOpen);
   return <div style={{ margin: '0 auto' }} >
-
-    <h2>Sidebar: { props.isOpen ? 'open' : 'false' }</h2>
-    <button className='btn btn-default' onClick={props.isOpen ? props.closeSidebar : props.openSidebar}>
-    {props.isOpen ? 'Close' : 'Open'} Sidebar
+    <h2>The sidebar is {props.isOpen ? 'open!' : 'closed.'}</h2>
+    <button className={'btn btn-' + (props.isOpen ? 'red' : 'teal')} onClick={props.isOpen ? props.closeSidebar : props.openSidebar}>
+    {props.isOpen ? 'Close' : 'Open'} it{props.isOpen ? '. ☹️ ' : '! 😄'}
     </button>
+    <div className={'sidebar padding-medium ' + (props.isOpen ? 'is-open' : '')}>
+      <button className='btn btn-default' onClick={props.closeSidebar}>
+      Close Sidebar 👻
+      </button>
+    </div>
   </div>
 }
 
